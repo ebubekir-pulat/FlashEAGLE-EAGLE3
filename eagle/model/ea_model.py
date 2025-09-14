44304a4973214f9ca864416199f63b0f0e565f8d
Ebubekir <ebubekirpulat15@gmail.com>
2025-09-14 22:05:36 +0930

    T

--- eagle/model/ea_model.py
+++ eagle/model/ea_model.py
@@ -257,7 +257,11 @@ class EaModel(nn.Module):
             print(self.past_key_values[0])
             print(self.past_key_values[0][0])
             print(np.shape(self.past_key_values[0][0].data))
-            self.past_key_values = [self.past_key_values[1]]
+
+            if len(self.past_key_values) == 2:
+                self.past_key_values = [self.past_key_values[1]]
+            else:
+                self.past_key_values = [self.past_key_values[0]]
 
 
 
