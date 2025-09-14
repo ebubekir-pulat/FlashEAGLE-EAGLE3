f0884642be7f793c89be2aae7155ea48df561ce7
Ebubekir <ebubekirpulat15@gmail.com>
2025-09-14 23:12:55 +0930

    T

--- eagle/model/ea_model.py
+++ eagle/model/ea_model.py
@@ -271,8 +271,8 @@ class EaModel(nn.Module):
             #else:
             #    self.past_key_values = [self.past_key_values[0]]
             print(len(self.past_key_values_data[0]))
-            self.past_key_values_data[0] = sample(list(self.past_key_values_data[0]), 100)
-            self.past_key_values_data[1] = sample(list(self.past_key_values_data[1]), 100)
+            self.past_key_values_data[0] = sample(list(self.past_key_values_data[0]), 10)
+            self.past_key_values_data[1] = sample(list(self.past_key_values_data[1]), 10)
 
 
             # with Timer("all"):
