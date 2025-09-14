4234bc9dbaf3152804424d0d33114361e43a96fe
Ebubekir <ebubekirpulat15@gmail.com>
2025-09-14 22:44:39 +0930

    T

--- eagle/model/ea_model.py
+++ eagle/model/ea_model.py
@@ -253,7 +253,7 @@ class EaModel(nn.Module):
         for idx in range(max_length):
             #print("Lens: ", self.current_length_data[0], end="\n\n\n")
             max_kv = max(max_kv, int(self.current_length_data[0]))
-            print(sys.getsizeof(self.past_key_values[0][1]))
+            print("\n\n", sys.getsizeof(self.past_key_values[0][1]))
             print(self.past_key_values[0])
             print(self.past_key_values[0][0])
             print(np.shape(self.past_key_values[0][0].data))
